002a462ef6f30739da121fabc3dd168ce1c45d6c
Daniel <spacegeek84@gmail.com>
2019-12-19 02:51:22 -0600

    finished faq and began working on the credit card form

--- faq.js
+++ faq.js
@@ -1,7 +1,6 @@
-$(document).ready(function () {
-    $('div.wrapper').find('div').toggleClass('small-div');
-});
-
-$('#click').click(function () {
-    $('div.wrapper').find('div').toggleClass('small-div big-div');
-});
+const expandableItems = document.querySelectorAll('.expandable').forEach(item => {
+    item.addEventListener('click', function () {
+        this.classList.toggle("hidden");
+        $(this).css({ "height": "auto"});
+    });
+})
---
+++ pay.js
@@ -0,0 +1,4 @@
+window.onload = function () {
+    $('form').each(function () { this.reset() });
+}
+
